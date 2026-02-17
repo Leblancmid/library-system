6c269203966ab9baeac3703a8a88243a0dc0ce1a
Michael Adriane Nabong <nabongmichaeladriane@gmail.com>
2026-02-17 21:26:32 +0800

    loan fix

--- app/Http/Controllers/LoanController.php
+++ app/Http/Controllers/LoanController.php
@@ -1,10 +1,27 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Book;
+use App\Models\Member;
+use App\Models\Loan;
 use Illuminate\Http\Request;
 
 class LoanController extends Controller
 {
-    //
+    public function index()
+    {
+        $loans = Loan::with(['book','member'])
+            ->latest()
+            ->paginate(10);
+
+        $books = Book::orderBy('title')->get();
+        $members = Member::orderBy('name')->get();
+
+        return view('loans.index', compact('loans', 'books', 'members'));
+    }
+
+    public function borrow(Request $request)
+    {
+        
+    }
 }
